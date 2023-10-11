952f82807d5ed4674762b1e02c561fa3855a1b1e
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2023-10-11 13:59:33 -0400

    Disable Extensions (in-dev)

--- src/consts.tsx
+++ src/consts.tsx
@@ -5,6 +5,6 @@ const github = "https://github.com/TheRealGeoDash2019/Octium";
 const discord = "https://discord.gg/25NxvxyEEr";
 const internalNamespace = "octium"; // original: cobalt (cobalt://, "://" omitted)
 const jsNamespace = "Octium"; // original: Cobalt (eg. Cobalt.web)
-const exposedInternalUrls = ["home", "blank", "settings", `${internalNamespace}-urls`, `extensions`]
+const exposedInternalUrls = ["home", "blank", "settings", `${internalNamespace}-urls`, /*`extensions`*/]
 
 export { obfuscation, bareServerURL, github, discord, internalNamespace, jsNamespace, exposedInternalUrls };
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { jsNamespace } from "./consts.tsx";
 import 'regenerator-runtime/runtime';
 import App from "./pages/app.js";
-import ExtensionManager from "./extensionManager.tsx";
+// import ExtensionManager from "./extensionManager.tsx";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 
-window[`${jsNamespace}ExtensionManager`] = new ExtensionManager();
+// window[`${jsNamespace}ExtensionManager`] = new ExtensionManager();
 
 root.render(
     <BrowserRouter basename="">
